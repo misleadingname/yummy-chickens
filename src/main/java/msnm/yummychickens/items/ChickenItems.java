303914c6a09a9fdda20883add8a0652ab1f2d4ac
misname <49401941+misleadingname@users.noreply.github.com>
2025-05-04 22:30:08 +0200

    oh ma gawd.

--- src/main/java/msnm/yummychickens/items/ChickenItems.java
+++ src/main/java/msnm/yummychickens/items/ChickenItems.java
@@ -2,6 +2,8 @@ package msnm.yummychickens.items;
 
 import msnm.yummychickens.YummyChickens;
 import net.minecraft.core.item.ItemFood;
+import net.minecraft.core.util.collection.NamespaceID;
+import turniplabs.halplibe.helper.ItemBuilder;
 import turniplabs.halplibe.helper.recipeBuilders.RecipeBuilderFurnace;
 
 public class ChickenItems {
@@ -11,25 +13,27 @@ public class ChickenItems {
 	public static RecipeBuilderFurnace RAW_CHICKEN_FURNACE_RECIPE;
 
 	public static void ConstructItems() {
-		RAW_CHICKEN = new ItemFood(
+		RAW_CHICKEN = new ItemBuilder(YummyChickens.MOD_ID)
+			.build(new ItemFood(
 			"rawChicken",
-			"yummyChickens:rawChicken",
+			NamespaceID.getPermanent("yummyChickens", "rawChicken").toString(),
 			20000,
 			4,
 			18,
 			false,
 			2
-		);
+		));
 
-		COOKED_CHICKEN = new ItemFood(
-			"cookedChicken",
-			"yummyChickens:cookedChicken",
-			20001,
-			13,
-			14,
-			false,
-			2
-		);
+		COOKED_CHICKEN = new ItemBuilder(YummyChickens.MOD_ID)
+			.build(new ItemFood(
+				"cookedChicken",
+				NamespaceID.getPermanent("yummyChickens", "cookedChicken").toString(),
+				20001,
+				13,
+				14,
+				false,
+				2
+			));
 	}
 
 	public static void ConstructRecipes() {
